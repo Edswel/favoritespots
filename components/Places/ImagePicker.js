4bbaf0708e7727c650f1eb982729f9f075843f49
Edswel <franckini@yahoo.com>
2022-05-17 10:28:56 +0100

    Manage permissions and take photo on iOS

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,8 +1,27 @@
-import { View, Button } from 'react-native';
-import { launchCameraAsync } from 'expo-image-picker';
+import { View, Button, Alert } from 'react-native';
+import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
 
 function ImagePicker() {
+    const [camePermissionInformation, requestPermission] = useCameraPermissions();
+
+    async function verifyPermissions() {
+        if (camePermissionInformation.status === PermissionStatus.UNDETERMINED) {
+            const permissionResponse = await requestPermission();
+
+            return permissionResponse.granted;
+        }
+        if (camePermissionInformation.status === PermissionStatus.DENIED) {
+            Alert.alert('Permission denied, action terminated!');
+            return false;
+        }
+        return true;
+    }
+
     async function takeImageHandler() {
+        const hasPermission = await verifyPermissions();
+        if (!hasPermission) {
+            return;
+        }
         const image = await launchCameraAsync({
             allowsEditing: true,
             aspect: [16, 9],
