3425edd60779f0d5fd9651946803cc36fff5303f
Edswel <franckini@yahoo.com>
2022-05-19 09:41:32 +0100

    Add location preview map

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,9 +1,12 @@
-import { View, StyleSheet, Alert } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet, Image, Alert } from 'react-native';
 import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from 'expo-location';
 
 import OutlinedButton from '../UI/OutlinedButton';
+import { getMapPreview } from '../../utils/location';
 
 function LocationPicker() {
+    const [pickedLocation, setPickedLocation] = useState();
     const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
 
     async function verifyPermissions() {
@@ -25,14 +28,23 @@ function LocationPicker() {
             return;
         }
         const location = await getCurrentPositionAsync();
-        console.log(location);
+        setPickedLocation({
+            lat: location.coords.latitude,
+            lng: location.coords.longitude
+        });
     }
 
     function pickOnMapHandler() { }
 
+    let locationPreview = <Text>No location picked yet.</Text>
+
+    if (pickedLocation) {
+        locationPreview = <Image style={styles.image} source={{ uri: getMapPreview(pickedLocation.lat, pickedLocation.lng) }} />
+    }
+
     return (
         <View>
-            <View style={styles.mapPreview}></View>
+            <View style={styles.mapPreview}>{locationPreview}</View>
             <View style={styles.actions}>
                 <OutlinedButton icon='location' onPress={getLocationHandler}>Locate User</OutlinedButton>
                 <OutlinedButton icon='map' onPress={pickOnMapHandler}>Pick on Map</OutlinedButton>
@@ -57,5 +69,10 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center'
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 4
     }
 });
